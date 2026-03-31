8872a083349fad076bd90eb4b1d318e9991868f7
Curel-X <jasoncomras@aol.com>
2026-03-31 03:46:20 -0400

    Allow for scroll still, fixing header change on scroll soon, style changes for scroll vs arrows for traversal, misc changes

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import React, {useState, useRef, useEffect, useLayoutEffect} from 'react';
 
 //IMPORT CORRESPONDING CSS SHEET
 import './Calendar.css';
@@ -17,12 +17,49 @@ import SunsetSunriseClearSky from './weather_backgrounds/SunsetSunriseClearSky.p
 import SunsetSunriseCloudy from './weather_backgrounds/SunsetSunriseCloudy.jpg';
 import SunsetSunrisePartlyCloudy from './weather_backgrounds/SunsetSunrisePartlyCloudy.jpg';
 
+//ICON UI
+import UpArrow from './icons/arrow-big-up.svg';
+import DownArrow from './icons/arrow-big-down.svg';
+
 //MAIN EXPORTED FUNCTION
 function Calendar({totalNumMonths}){
+
+    //GENERAL DATE OBJECT
+    const date = new Date();
 
     //BACKGROUND FOR THE GENERAL CALENDAR
     const [background, setBackground] = useState(null);
     const [backgroundWeather, setBackgroundWeather] = useState(-1);
+
+    //HOLDS MONTHS AWAY FROM NOW (CURRENT MONTH)
+    const [monthsFromNow, setMonthsFromNow] = useState(0);
+
+    //OBSERVER TO CHANGE MONTH HEADER BASED ON MAJORITY OF MONTH ON SCREEN
+    //BASICALLY IF MORE THAN HALF OF APRIL IS ON THE SCREEN AND LESS THAN HALF OF
+    //MAY IS ON SCREEN, APRIL WILL BE THE HEADER, ONCE MAY IS ON SCREEN MORE THAN HALF, THEN MAY
+
+    //TO BE DONE, AT THE MOMENT MONTH HEADERS DO NOT UPDATE PROPERLY WHEN SCROLLING ON MULTIPLE
+    // const containerRef = useRef();
+
+    // const handleScroll = () => {
+    //     const scrollTop = containerRef.current.scrollTop; // vertical
+    //     const monthHeight = containerRef.current.firstChild.offsetHeight; // assumes all months same height
+    //     const monthIndex = Math.round(scrollTop / monthHeight);
+    //     setMonthsFromNow(monthIndex);
+    // };
+
+    //COMPUTE THE FIRST DAY OF THE TARGET MONTH
+    const targetDate = new Date(date.getFullYear(), date.getMonth() + monthsFromNow, 1);
+    // THE CURRENT MONTH'S NAME
+    const monthName = targetDate.toLocaleString('default', { month: 'long' });
+    // CURRENT YEAR (YYYY FORMAT)
+    const year = targetDate.getFullYear();    
+
+    //ARRAY OF WEEKDAY NAMES FOR TITLES
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    //TBD WHEN YOU CLICK THE MONTH/YEAR HEADER
+    const [monthDropdown, setMonthDropdown] = useState(false);
 
      //GETS THE CURRENT TIME OF DAY
     function getTimeOfDay() {
@@ -67,15 +104,56 @@ function Calendar({totalNumMonths}){
         setBackground(getWeatherImg(backgroundWeather));
     }, [backgroundWeather]);
 
+    
     //HTML DOM RETURN
     return(
         //BACKGROUND WITH THE CURRENT BACKGROUND IMAGE EMBEDDED IN STYLE
         <div className = "calendar-calendar-background" style={{ '--bg-img': `url(${background}`}}>
+
+            {/* THE INTERACTABLE HEADER FOR CALENDAR */}
+            <div className="calendar-month-interactable-header">
+                {/* LEFT ARROW TO DECREMENT BY A MONTH (ONLY FOR ONE MONTH) */}
+                {totalNumMonths === 1 && <img onClick = {()=>setMonthsFromNow(monthsFromNow - 1)} className = "calendar-month-arrow" src = {UpArrow}/>}
+                {/* MONTH NAME CORESPONDING TO CURRENT MONTH */}
+                <h1 className = "calendar-month-month-name" onClick = {()=>{totalNumMonths === 1 && setMonthDropdown(!monthDropdown)}}>{monthName + " " + year}</h1>
+                {/* LEFT ARROW TO INCREMENT BY A MONTH (ONLY FOR ONE MONTH) */}
+                {totalNumMonths === 1 && <img onClick = {()=>setMonthsFromNow(monthsFromNow + 1)} className = "calendar-month-arrow" src = {DownArrow}/>}
+            </div>
+
+            {/* WEEKDAY HEADER (MONDAY, TUESDAY, ...) */}
+            <div className="calendar-weekdays">
+                {/* SHOW THE WEEKDAYS ON THE TOP */}
+                {weekdays.map((day) => (
+                <div key={day} className="weekday">
+                    {day}
+                </div>
+                ))}
+            </div>
+
             {/* WRAPPER FOR CALENDAR MONTHS TO BE STACKED PROPERLY */}
             {/* SHOW THE CALENDARS FOR FIRST 5 MONTHS */}
             {Array.from({ length: totalNumMonths }, (_, i) => (
-                <CalendarMonth key={i} monthsAwayFromNow={i} singleMonth = {totalNumMonths === 1} setBackgroundWeather = {setBackgroundWeather}/>
+                <div key={i} data-index={i} className="calendar-month">
+                    <CalendarMonth key={i} monthsFromNow = {monthsFromNow + (i)} setBackgroundWeather = {setBackgroundWeather} singleMonth = {totalNumMonths === 1}/>
+                </div>
             ))}
+
+            {/* <div
+                className="calendar-scroll-container"
+                ref={containerRef}
+                onScroll={handleScroll} 
+            >
+                {Array.from({ length: totalNumMonths }, (_, i) => (
+                <div key={i} className="calendar-month">
+                    <CalendarMonth
+                    monthsFromNow={i}
+                    setBackgroundWeather={setBackgroundWeather}
+                    singleMonth={totalNumMonths === 1}
+                    />
+                </div>
+                ))}
+            </div> */}
+
         </div>
     );
 
--- src/CalendarMonth.jsx
+++ src/CalendarMonth.jsx
@@ -2,13 +2,13 @@ import './CalendarMonth.css';
 
 //DAY GRID FOR THE CALENDAR
 import DayGrid from './DayGrid.jsx';
-import Weather from './Weather.jsx'
+import Weather from './Weather.jsx';
 
 //USE STATE AND EFFECT FOR AUTO UPDATES & DOM RELOAD SAVE
 import { useState, useEffect } from 'react';
 
 //MAIN CONSTRUCTOR FOR CALENDAR MONTH
-function CalendarMonth({monthsAwayFromNow, singleMonth, setBackgroundWeather}){
+function CalendarMonth({monthsFromNow, setBackgroundWeather, singleMonth}){
 
     //HOLDS THE CURRENT WEATHER STATE FOR EXTENDED TIME
     const [weather, setWeather] = useState(null);
@@ -16,30 +16,19 @@ function CalendarMonth({monthsAwayFromNow, singleMonth, setBackgroundWeather}){
     //HOLDS DATE GENERAL OBJECT
     const date = new Date();
 
-    //HOLDS MONTHS AWAY FROM NOW (CURRENT DATE)
-    const [monthsFromNow, setMonthsFromNow] = useState(monthsAwayFromNow);
-
     //COMPUTE THE FIRST DAY OF THE TARGET MONTH
     const targetDate = new Date(date.getFullYear(), date.getMonth() + monthsFromNow, 1);
     // HOW MANY DAYS ARE WITHIN THIS MONTH
     const daysInMonth = new Date(targetDate.getFullYear(), targetDate.getMonth() + 1, 0).getDate();
     // GRID INDENT OF THE FIRST DAY FOR CALENDAR
     const firstDay = targetDate.getDay();
-    // THE CURRENT MONTH'S NAME
-    const monthName = targetDate.toLocaleString('default', { month: 'long' });
     // CURRENT YEAR (YYYY FORMAT)
     const year = targetDate.getFullYear();
 
     //MAXIMUM WEATHER DAYS DISPLAYED
     const maxFutureWeatherDays = 30;
     const maxPastWeatherDays = 30;
-
-    //ARRAY OF WEEKDAY NAMES FOR TITLES
-    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     
-    //TBD WHEN YOU CLICK THE MONTH/YEAR HEADER
-    const [monthDropdown, setMonthDropdown] = useState(false);
-
     //GETS THE CURRENT WEATHER IN REAL TIME
     const [currentWeather, setCurrentWeather] = useState(-1);
 
@@ -58,7 +47,7 @@ function CalendarMonth({monthsAwayFromNow, singleMonth, setBackgroundWeather}){
         );
 
     // EMPTY DAYS (OFFSET THAT REPLACES INDENTATION)
-    const prevMonthDays = Array.from({ length: firstDay }, (_, i) => {
+    const prevMonthDays = singleMonth ? Array.from({ length: firstDay }, (_, i) => {
 
         //OBTAIN THE PRIOR MONTH DATE TO GET THE DAYS IN PRIOR MONTH AND THEN SUBTRACT FROM THIS MONTHS FIRST DAY
         const prevMonthDate = new Date(year, date.getMonth() + monthsFromNow, 0);
@@ -68,7 +57,7 @@ function CalendarMonth({monthsAwayFromNow, singleMonth, setBackgroundWeather}){
         //RETURN A COPY OF LAST MONTHS PRIOR DAYS FILLING UP TO CURRENT NEW MONTH DAY (GRAYED OUT)
         return (<DayGrid key={`prev-${i}`} setCurrentWeather={setCurrentWeather} weather={weather} maxFutureWeatherDays={maxFutureWeatherDays} maxPastWeatherDays={maxPastWeatherDays} dayOfMonth={day} year={year} month={date.getMonth() + monthsFromNow - 1} isOtherMonth={true}/>);
     
-    });
+    }) : [];
 
     //COMBINE EMPTY DAYS WITH DAYS ARR
     const dayArr = [...prevMonthDays, ...realDays];
@@ -81,26 +70,8 @@ function CalendarMonth({monthsAwayFromNow, singleMonth, setBackgroundWeather}){
             {/* WRAPPER FOR THE MAIN CALENDAR THAT HOLDS THE ARRAY OF DAYS */}
             {/* style={{ '--bg-img': `url(${getWeatherImg(currentWeather)})` }} */}
             <div className="calendar-month-wrapper"> 
-                {/* THE INTERACTABLE HEADER FOR CALENDAR */}
-                <div className="calendar-month-interactable-header">
-                    {/* LEFT ARROW TO DECREMENT BY A MONTH (ONLY FOR ONE MONTH) */}
-                    {singleMonth && <h1 onClick = {()=>setMonthsFromNow(monthsFromNow - 1)} className = "calendar-month-arrow">{"←"}</h1>}
-                    {/* MONTH NAME CORESPONDING TO CURRENT MONTH */}
-                    <h1 className = "calendar-month-month-name" onClick = {()=>{singleMonth && setMonthDropdown(!monthDropdown)}}>{monthName + " " + year}</h1>
-                    {/* LEFT ARROW TO INCREMENT BY A MONTH (ONLY FOR ONE MONTH) */}
-                    {singleMonth && <h1 onClick = {()=>setMonthsFromNow(monthsFromNow + 1)} className = "calendar-month-arrow">{"→"}</h1>}
-                </div>
-                {/* WEEKDAY HEADER (MONDAY, TUESDAY, ...) */}
-                <div className="calendar-weekdays">
-                    {/* SHOW THE WEEKDAYS ON THE TOP */}
-                    {weekdays.map((day) => (
-                    <div key={day} className="weekday">
-                        {day}
-                    </div>
-                    ))}
-                </div>
                 {/* DAYGRID CELLS INDENTED BASED ON START DATE */}
-                <div className="calendar-month-day-grid-wrapper" style = {{"--first-day" : firstDay + 1}}>  
+                <div className="calendar-month-day-grid-wrapper" style = {{"--first-day" : !singleMonth ? firstDay + 1 : 0}}>  
                     {dayArr}
                 </div>
             </div>
