36931e605b3800b42eb0fab897897cd47fa03c50
Bear112006 <jonathanslattery112006@gmail.com>
2026-04-07 00:50:33 -0400

    Repair main.dart syntax errors

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
-// Import the file you just created. Ensure the path matches your folder structure!
 import 'screens/login_screen.dart';
+import 'theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Calendar++',
-      // This hides the little "DEBUG" banner in the top right corner
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // This is the most important line: it tells Flutter what screen to show first
-      home: LoginScreen(),
+      theme: AppTheme.build(),
+      home: const LoginScreen(),
     );
   }
 }
